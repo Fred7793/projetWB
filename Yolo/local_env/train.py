2173fcdba3314c262b5e2ea3067e5b5c611f1606
Guillaume <131155392+Guillaume-amann@users.noreply.github.com>
2024-06-06 12:12:59 +0100

    Delete Yolo directory

--- Yolo/local_env/train.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('yolov8m.yaml')  # load a pretrained model (recommended for training)
-
-model.train(data='/Users/fredericdai/Desktop/Projet/Yolo/config.yaml',epochs=100)
-# Evaluate the model's performance on the validation set
-results = model.val()
